f6a462afb86f5b30c1d658cfce05ad91bf19ab34
Elka Dimitrova <dimitrova_elka@abv.bg>
2018-04-15 23:07:30 +0300

    Part I - VI

---
+++ Forum.App/Contracts/Services/PostService.cs
@@ -0,0 +1,49 @@
+﻿namespace Forum.App.Contracts.Services
+{
+    using Forum.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class PostService : IPostService
+    {
+        private ForumData forumData;
+        private ISession session;
+
+        public PostService(ForumData forumData, ISession session)
+        {
+            this.forumData = forumData;
+            this.session = session;
+        }
+
+        public int AddPost(int userId, string postTitle, string postCategory, string postContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddReplyToPost(int postId, string replyContents, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ICategoryInfoViewModel> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCategoryName(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IPostInfoViewModel> GetCategoryPostsInfo(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostViewModel GetPostViewModel(int postId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Forum.App/Contracts/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿namespace Forum.App.Contracts.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Forum.Data;
+    using Forum.DataModels;
+
+    public class UserService : IUserService
+    {
+        private ForumData forumData;
+        private ISession session;
+
+        public UserService(ForumData forumData, ISession session)
+        {
+            this.forumData = forumData;
+            this.session = session;
+        }
+
+        public User GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUserName(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryLogInUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TrySignUpUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
